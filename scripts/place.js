b8b9b00a8b8cb74c01cf5d5435bb700e22b42f60
Kerlando Fucien <kfucien@byupathway.edu>
2026-07-21 02:51:47 -0400

    w03 assignment

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,24 @@
-const year = document .querySelector("#currentyear");
+const year = document.querySelector("#currentyear");
 const lasModified = document.querySelector("#lastModified");
 
 year.textContent = new Date().getFullYear();
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const temperature = 10;
+const windSpeed = 5
+
+function calculateWindChill(temp,speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed,0.16)
+    ).toFixed(1)
+}
+let windChill = "N/A";
+
+if (temperature <= 10 && windSpeed> 4.8) {
+    windChill = `${calculateWindChill(temperature,windSpeed)} °C`;
+}
+
+document.querySelector("#wind-chill").textContent = windChill;
